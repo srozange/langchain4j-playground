af9d1db8ae4c37f3d484a02c706d212efc923ff6
Sébastien Rozange <sebastien.rozange@accenture.com>
2025-11-30 20:32:07 +0100

    log du tool call dans l'interface graphique pour le module tool

--- langchain4j-playground-tools/src/main/java/org/srozange/langchain4j/ChatBotWebSocket.java
+++ langchain4j-playground-tools/src/main/java/org/srozange/langchain4j/ChatBotWebSocket.java
@@ -1,9 +1,13 @@
 package org.srozange.langchain4j;
 
+import dev.langchain4j.observability.api.event.ToolExecutedEvent;
+import io.quarkiverse.langchain4j.observability.AiServiceSelector;
 import io.quarkus.websockets.next.OnOpen;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
 import io.quarkus.websockets.next.WebSocketConnection;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -12,8 +16,11 @@ public class ChatBotWebSocket {
 
     private final Bot bot;
 
-    public ChatBotWebSocket(Bot bot) {
+    private final WebSocketConnection connection;
+
+    public ChatBotWebSocket(Bot bot, WebSocketConnection connection) {
         this.bot = bot;
+        this.connection = connection;
     }
 
     @OnOpen
@@ -25,4 +32,10 @@ public class ChatBotWebSocket {
     public String onMessage(String message, WebSocketConnection connection) {
         return ChatUtils.computeChatResponse(message, (msg) -> bot.chat(connection.id(), msg));
     }
+
+    public void botToolExecuted(@Observes @AiServiceSelector(Bot.class) ToolExecutedEvent toolExecutedEvent) {
+        connection.sendTextAndAwait("<i style=\"color: blueviolet;\">Tool called :<ul><li>name : " + toolExecutedEvent.request().name()
+                + "</li><li>arguments : " + toolExecutedEvent.request().arguments()
+                + "</li><li>result : " + toolExecutedEvent.resultText() + "</li></ul></i>");
+        }
 }
